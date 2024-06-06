70595ed3b4fe7fb836a3def772a5aad8d4b3614f
CheolJe <me@falsy.me>
2024-06-06 14:40:25 +0900

    feat: remove core codes

--- src/core/domains/usecases/interfaces/ICarrierUseCase.ts
+++
@@ -1,6 +0,0 @@
-import ICarrierDTO from "../../../dtos/interfaces/ICarrierDTO"
-import ILayerDTO from "../../../dtos/interfaces/ILayerDTO"
-
-export default interface ICarrierUseCase {
-  getCarriers(): Promise<ILayerDTO<ICarrierDTO[]>>
-}
--- src/core/repositories/interfaces/ICarrierRepository.ts
+++
@@ -1,7 +0,0 @@
-import ICarrierDTO from "../../dtos/interfaces/ICarrierDTO"
-import ILayerDTO from "../../dtos/interfaces/ILayerDTO"
-
-export default interface ICarrierRepository {
-  getCarriers?(): Promise<ILayerDTO<ICarrierDTO[]>>
-  getCarrier?(carrierId: string): Promise<ILayerDTO<ICarrierDTO>>
-}
